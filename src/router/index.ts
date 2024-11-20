734bf66ea7827964c741acd9a7924e145d84c78c
Bekzod Akbarov <stx.webdev@gmail.com>
2024-11-20 15:53:37 +0500

    setup auto routes in vue router with typescript support

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,35 +1,9 @@
-import { h } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router/auto';
+import { routes } from 'vue-router/auto-routes';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/HomeView.vue'),
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('@/views/ProjectsView.vue'),
-    },
-    {
-      path: '/projects/:id',
-      name: 'single-project',
-      component: () => import('@/views/SingleProject.vue'),
-    },
-    {
-      path: '/:catchAll(.*)*',
-      name: 'not-found',
-      component: {
-        render() {
-          return h('div', 'Not Found');
-        },
-        displayName: 'NotFound',
-      },
-    },
-  ],
+  routes,
 });
 
 export default router;
