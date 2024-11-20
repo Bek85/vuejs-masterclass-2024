d55326bd6488f70b7838bc3c49f28c26cff0a229
Bekzod Akbarov <stx.webdev@gmail.com>
2024-11-20 17:27:53 +0500

    secure secret app data with vite env variables

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  'https://bkqbyveibedqpgiahaur.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrcWJ5dmVpYmVkcXBnaWFoYXVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIxMDEyMDgsImV4cCI6MjA0NzY3NzIwOH0.aEcy88aLDQ834POqEvSZ-aPcBxOjeEjtsfAPKUmwXlA',
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
 );
