634f2fe7248c2e39a6cdb6accfc225f088a62874
Bekzod Akbarov <stx.webdev@gmail.com>
2024-11-21 14:43:05 +0500

    utilize shadcn input and dropdown components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,50 @@
 <script setup lang="ts">
+import { Input } from '@/components/ui/input';
+import {
+  LogOut,
+  User,
+} from 'lucide-vue-next';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 </script>
 <template>
   <nav class="h-16 border-b bg-muted/40 flex justify-between items-center px-6">
     <form class="w-full max-w-96">
-      <input type="text" class="w-full bg-background pl-8" placeholder="Search..." />
+      <Input type="text" placeholder="Search..." />
     </form>
-    <div class="w-8 h-8 rounded-full bg-white"></div>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent class="w-56">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <User class="mr-2 h-4 w-4" />
+            <span>Profile</span>
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <LogOut class="mr-2 h-4 w-4" />
+            <span>Log out</span>
+            <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
   </nav>
   <main>
     <RouterView />
