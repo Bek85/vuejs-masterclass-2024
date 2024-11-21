e2209a7487949f50b46191481e19bc9b93aeefb3
Bekzod Akbarov <stx.webdev@gmail.com>
2024-11-21 18:38:30 +0500

    fix

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -5,69 +5,56 @@ import type { Tables } from '@root/database/types/database.types';
 import type { ColumnDef } from '@tanstack/vue-table';
 import DataTable from '@/components/ui/data-table/DataTable.vue';
 
-const tasks = ref<Tables<'tasks'>[] | null>([]);
+const tasks = ref<Tables<'tasks'>[] | null>(null)
+  ; (async () => {
+    const { data, error } = await supabase.from('tasks').select()
 
-// this needs to be anonymous function
-(async () => {
+    if (error) console.log(error)
 
-  const { data, error } = await supabase.from('tasks').select('*');
+    tasks.value = data
+  })()
 
-  if (error) console.log(error);
-
-  tasks.value = data;
-
-})();
-
-const columns: ColumnDef<Tables<'tasks'>[]>[] = [
-  // ID Column
-  {
-    accessorKey: 'id',
-    header: () => h('div', { class: 'text-left' }, 'ID'),
-    cell: ({ row }) => h('div', { class: 'text-left' }, row.getValue('id')),
-  },
-
-  // Name Column
+const columns: ColumnDef<Tables<'tasks'>>[] = [
   {
     accessorKey: 'name',
     header: () => h('div', { class: 'text-left' }, 'Name'),
-    cell: ({ row }) => h('div', { class: 'text-left' }, row.getValue('name')),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('name'))
+    }
   },
-
-  // Status Column
   {
     accessorKey: 'status',
     header: () => h('div', { class: 'text-left' }, 'Status'),
-    cell: ({ row }) => h('div', { class: 'text-left' }, row.getValue('status')),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
+    }
   },
-  // Description Column
-  {
-    accessorKey: 'description',
-    header: () => h('div', { class: 'text-left' }, 'Description'),
-    cell: ({ row }) => h('div', { class: 'text-left' }, row.getValue('description')),
-  },
-  // Due Date Column
   {
     accessorKey: 'due_date',
     header: () => h('div', { class: 'text-left' }, 'Due Date'),
-    cell: ({ row }) => h('div', { class: 'text-left' }, row.getValue('due_date')),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('due_date'))
+    }
   },
-  // Project ID Column
   {
     accessorKey: 'project_id',
-    header: () => h('div', { class: 'text-left' }, 'Project ID'),
-    cell: ({ row }) => h('div', { class: 'text-left' }, row.getValue('project_id')),
+    header: () => h('div', { class: 'text-left' }, 'Project'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('project_id'))
+    }
   },
-  // Collaborators Column
   {
     accessorKey: 'collaborators',
     header: () => h('div', { class: 'text-left' }, 'Collaborators'),
-    cell: ({ row }) => h('div', { class: 'text-left' }, JSON.stringify(row.getValue('collaborators'))),
+    cell: ({ row }) => {
+      return h(
+        'div',
+        { class: 'text-left font-medium' },
+        JSON.stringify(row.getValue('collaborators'))
+      )
+    }
   }
 ]
-
-
-
-
 </script>
 <template>
   <DataTable v-if="tasks" :columns="columns" :data="tasks" />
