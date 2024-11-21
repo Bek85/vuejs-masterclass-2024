4bfb3822f121c58ecc06711f7470e20709caf68f
Bekzod Akbarov <stx.webdev@gmail.com>
2024-11-21 14:15:54 +0500

    create basic app navbar layout with tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient';
-
-console.log(supabase);
 </script>
 <template>
+  <nav class="h-16 border-b bg-muted/40 flex justify-between items-center px-6">
+    <form class="w-full max-w-96">
+      <input type="text" class="w-full bg-background pl-8" placeholder="Search..." />
+    </form>
+    <div class="w-8 h-8 rounded-full bg-white"></div>
+  </nav>
   <main>
     <RouterView />
   </main>
