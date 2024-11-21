6a245b3543953c3a32d465be65ca1b795623afd1
Bekzod Akbarov <stx.webdev@gmail.com>
2024-11-21 18:46:18 +0500

    create a data table for the projects page

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -2,13 +2,16 @@
 import { supabase } from '@/lib/supabaseClient';
 import { ref } from 'vue';
 import type { Tables } from '@root/database/types/database.types';
+import { h } from 'vue';
+import type { ColumnDef } from '@tanstack/vue-table';
+import DataTable from '@/components/ui/data-table/DataTable.vue';
 
-const projects = ref<Tables<'projects'>[] | null>([]);
+const projects = ref<Tables<'projects'>[] | null>(null);
 
 // this needs to be anonymous function
 (async () => {
 
-  const { data, error } = await supabase.from('projects').select('*');
+  const { data, error } = await supabase.from('projects').select();
 
   if (error) console.log(error);
 
@@ -16,12 +19,32 @@ const projects = ref<Tables<'projects'>[] | null>([]);
 
 })();
 
+const columns: ColumnDef<Tables<'projects'>>[] = [
+  {
+    accessorKey: 'id',
+    header: () => h('div', { class: 'text-left' }, 'ID'),
+    cell: ({ row }) => h('div', { class: 'text-left' }, row.getValue('id')),
+  },
+  // Name Column
+  {
+    accessorKey: 'name',
+    header: () => h('div', { class: 'text-left' }, 'Name'),
+    cell: ({ row }) => h('div', { class: 'text-left' }, row.getValue('name')),
+  },
+  {
+    accessorKey: 'status',
+    header: () => h('div', { class: 'text-left' }, 'Status'),
+    cell: ({ row }) => h('div', { class: 'text-left' }, row.getValue('status')),
+  },
+  {
+    accessorKey: 'collaborators',
+    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
+    cell: ({ row }) => h('div', { class: 'text-left' }, JSON.stringify(row.getValue('collaborators'))),
+  }
 
+]
 </script>
 <template>
-  <h1>Projects Page</h1>
-  <ul>
-    <li v-for="project in projects" :key="project.id"> {{ project.name }} </li>
-  </ul>
+  <DataTable v-if="projects" :columns="columns" :data="projects" />
 </template>
 <style scoped></style>
