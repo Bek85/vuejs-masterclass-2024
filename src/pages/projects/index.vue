06ef06e852fc1ac87b2ab054dff3e0bc1147d981
Bekzod Akbarov <stx.webdev@gmail.com>
2024-11-21 10:49:08 +0500

    use Immediately Invoked Function Expression(IIFE) in script setup

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,19 +1,17 @@
 <script setup lang="ts">
 import { supabase } from '@/lib/supabaseClient';
-import { onMounted } from 'vue';
 
-const fetchProjects = async () => {
+// this needs to be anonymous function
+(async () => {
   try {
     const { data: projects, error } = await supabase.from('projects').select('*');
     console.log(projects);
   } catch (error) {
     console.error(error);
   }
-};
+})();
 
-onMounted(async () => {
-  await fetchProjects();
-});
+
 </script>
 <template>
   <h1>Projects Page</h1>
