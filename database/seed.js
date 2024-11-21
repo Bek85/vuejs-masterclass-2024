5ef964fc8ca5d2cd21a6de797e10e9297ff3c8aa
Bekzod Akbarov <stx.webdev@gmail.com>
2024-11-21 10:07:31 +0500

    seedin the supabase remote database with Faker.js

--- database/seed.js
+++ database/seed.js
@@ -6,4 +6,16 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
 
-console.log(supabase);
+const seedProjects = async () => {
+  // need to insert data to supabase database
+  const name = faker.lorem.words(3);
+
+  await supabase.from('projects').insert({
+    name,
+    slug: name.toLowerCase().replace(/ /g, '-'),
+    status: faker.helpers.arrayElement(['in-progress', 'completed']),
+    collaborators: faker.helpers.arrayElements([1, 2, 3]),
+  });
+};
+
+await seedProjects();
