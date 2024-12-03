9a2423cc42cbe62c9b68ca4b4a500c8a19e31164
Bekzod Akbarov <stx.webdev@gmail.com>
2024-12-03 18:08:58 +0500

    use the vue.js Template special element with v-for loops

--- src/components/Layout/SidebarLinks.vue
+++ src/components/Layout/SidebarLinks.vue
@@ -6,23 +6,24 @@ interface LinkProp {
   icon: string;
 }
 
-const props = defineProps<{
+defineProps<{
   links: LinkProp[]
 }>();
 
-const filteredLinks = props.links.filter((link): link is LinkProp & { to: string } => !!link.to);
 
 </script>
 <template>
-  <RouterLink exact-active-class="text-white bg-muted" :key="link.title" v-for="link in filteredLinks" :to="link.to"
-    class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground">
-    <iconify-icon :icon="link.icon"></iconify-icon>
-    <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
-  </RouterLink>
-  <div class='nav-link cursor-pointer'>
-    <!-- <iconify-icon :icon="link.icon"></iconify-icon> -->
-    <!-- <span class="hidden lg:block text-nowrap">{{ link.title }}</span> -->
-  </div>
+  <template v-for="link in links" :key="link.title">
+    <RouterLink v-if="link.to" exact-active-class="text-white bg-muted" :to="link.to"
+      class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground">
+      <iconify-icon :icon="link.icon"></iconify-icon>
+      <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+    </RouterLink>
+    <div v-else class="nav-link cursor-pointer">
+      <iconify-icon :icon="link.icon"></iconify-icon>
+      <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+    </div>
+  </template>
 </template>
 <style scoped>
 .nav-link {
