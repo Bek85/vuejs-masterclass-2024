5d9c1e7cb8516cf728a3348e162b78ecff99d68c
Bekzod Akbarov <stx.webdev@gmail.com>
2024-12-17 12:22:42 +0500

    use useMemoize from VueUse to optimize pinia loader functions

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -1,13 +1,14 @@
 import type { Projects } from '@/utils/projectsQueries';
 import { projectsQuery } from '@/utils/projectsQueries';
+import { useMemoize } from '@vueuse/core';
 
 export const useProjectsStore = defineStore('projects-store', () => {
   const projects = ref<Projects | null>(null);
 
-  const fetchProjects = async () => {
-    if (projects.value?.length) return;
+  const loadProjects = useMemoize(async (key: string) => await projectsQuery);
 
-    const { data, error, status } = await projectsQuery;
+  const fetchProjects = async () => {
+    const { data, error, status } = await loadProjects('projects');
 
     if (error) useErrorStore().setError({ error, customCode: status });
 
